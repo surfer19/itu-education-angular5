7a65733fc682d8596c4135e907223c61fbb2019e
Unknown <matusburzala@gmail.com>
2017-12-09 22:51:53 +0100

    expert, advanced done

---
+++ src/app/advc/advc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advc',
+  templateUrl: './advc.component.html',
+  styleUrls: ['./advc.component.scss']
+})
+export class AdvcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/expr/expr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expr',
+  templateUrl: './expr.component.html',
+  styleUrls: ['./expr.component.scss']
+})
+export class ExprComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
